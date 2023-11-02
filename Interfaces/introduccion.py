c49ec3b91ea4fd945ac67d33f29e73bc9953bf58
YamilaP25 <145406493+YamilaP25@users.noreply.github.com>
2023-11-02 16:01:56 -0600

    creacion rama frontend (#2)

--- Interfaces/introduccion.py
+++ Interfaces/introduccion.py
@@ -1,4 +1,4 @@
-print('Hola mundo, esta es mi primer interfaz')
+print('Hola mundo, esta es mi primer interfaz, Disfrutalo!')
 
 a = 40
 b = 56
@@ -7,6 +7,10 @@ c = 89
 def suma():
     print(a + b + c)
 suma()
+
+def mostrar():
+    print('Esta es la creacion de la rama de frontend')
+mostrar()
 
 def imprime():
     print('Esta es la rama para desarrollar al backend')
